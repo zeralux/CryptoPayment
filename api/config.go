09c9091bf4ee8b05ec9c7545201120f2e184556b
david <161@18mail.com>
2022-08-15 17:30:18 +0800

    refactor

---
+++ api/config.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zeralux/CryptoPayment/api/constant"
+	"github.com/zeralux/CryptoPayment/api/controller/v1"
+)
+
+func GetRouter() *gin.Engine {
+	// gin start
+	router := gin.Default()
+
+	// 對外API, GateWay不檢查token
+	ver1Url := router.Group(constant.Ver1Url)
+	{
+		ver1Url.GET(constant.UsersUrl, v1.GetUsers)
+		ver1Url.GET(constant.UsersFileUrl, v1.GetUsersFile)
+	}
+
+	// 對外API, GateWay檢查token
+	privateUrl := router.Group(constant.PrivateUrl)
+	{
+		ver1Url := privateUrl.Group(constant.Ver1Url)
+		{
+			ver1Url.GET(constant.UserIdUrl, v1.GetUser)
+		}
+	}
+
+	// 對內API, 內網即可調用
+	internalUrl := router.Group(constant.InternalUrl)
+	{
+		ver1Url := internalUrl.Group(constant.Ver1Url)
+		{
+			ver1Url.POST(constant.UserUrl, v1.InsertUser)
+			ver1Url.PATCH(constant.UserIdUrl, v1.UpdateUser)
+			ver1Url.DELETE(constant.UserIdUrl, v1.DeleteUser)
+		}
+	}
+	
+	return router
+}
--- main.go
+++ main.go
@@ -2,63 +2,31 @@ package main
 
 import (
 	"flag"
-	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/zeralux/CryptoPayment/api/constant"
-	v1 "github.com/zeralux/CryptoPayment/api/controller/v1"
-	docs "github.com/zeralux/CryptoPayment/docs"
+	"github.com/zeralux/CryptoPayment/api"
+	"github.com/zeralux/CryptoPayment/docs"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 )
 
 var configLocation = flag.String("config.location", "application-local.yml", "config file location")
-var config *Config
+var AppConfig *ApplicationYml
 
 func main() {
 	flag.Parse()
-	// 讀取config
-	readConfig()
+	// 讀取yml
+	readAppConfig()
 
-	// gin start
-	router := gin.Default()
-
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router := api.GetRouter()
 	// swagger
 	docs.SwaggerInfo.BasePath = "/"
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
-	// 對外API, GateWay不檢查token
-	ver1Url := router.Group(constant.Ver1Url)
-	{
-		ver1Url.GET(constant.UsersUrl, v1.GetUsers)
-		ver1Url.GET(constant.UsersFileUrl, v1.GetUsersFile)
-	}
-
-	// 對外API, GateWay檢查token
-	privateUrl := router.Group(constant.PrivateUrl)
-	{
-		ver1Url := privateUrl.Group(constant.Ver1Url)
-		{
-			ver1Url.GET(constant.UserIdUrl, v1.GetUser)
-		}
-	}
-
-	// 對內API, 內網即可調用
-	internalUrl := router.Group(constant.InternalUrl)
-	{
-		ver1Url := internalUrl.Group(constant.Ver1Url)
-		{
-			ver1Url.POST(constant.UserUrl, v1.InsertUser)
-			ver1Url.PATCH(constant.UserIdUrl, v1.UpdateUser)
-			ver1Url.DELETE(constant.UserIdUrl, v1.DeleteUser)
-		}
-	}
-
-	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	router.Run()
 }
 
-type Config struct {
+type ApplicationYml struct {
 	//Wallet struct {
 	//	Limit struct {
 	//		Address map[string]int64 `yaml:",flow"`
@@ -66,14 +34,14 @@ type Config struct {
 	//}
 }
 
-func readConfig() {
+func readAppConfig() {
 	var err error
 	var bytes []byte
 	if bytes, err = ioutil.ReadFile(*configLocation); err != nil {
 		panic(err)
 	}
-	config = &Config{}
-	if err = yaml.Unmarshal(bytes, config); err != nil {
+	AppConfig = &ApplicationYml{}
+	if err = yaml.Unmarshal(bytes, AppConfig); err != nil {
 		panic(err)
 	}
 }
